7d711140913a181b714be0742f273f46d2a03340
Julio Longo <[julio.andrade321@gmail.com]>
2020-06-23 00:33:40 -0300

    .

--- db/mysqldb.ts
+++ db/mysqldb.ts
@@ -1,10 +1,10 @@
 import { Client } from "https://deno.land/x/mysql/mod.ts";
 
 const client = await new Client().connect({
-  hostname: "162.241.2.192",
-  username: "pointn27_julioxp",
-  db: "pointn27_anime",
-  password: "123456",
+  hostname: "",
+  username: "",
+  db: "",
+  password: "",
 });
 
 export default client;
